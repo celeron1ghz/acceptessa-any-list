1f4f16fc1f5514b7f3b06cd62571d11cd9c7303b
celeron1ghz <celeron1ghz@gmail.com>
2024-06-06 16:59:50 +0900

    add test routing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 // import { useState } from 'react'
+import { Router, Route, Link } from 'wouter';
+import { useHashLocation } from 'wouter/use-hash-location';
 import './App.css'
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
+    <Router hook={useHashLocation}>
+      <h1 className="text-3xl font-bold underline mb-6">
         Hello world!
       </h1>
-
-    </>
+      <Link href='/test' className="bg-blue-600 hover:bg-blue-500 text-white rounded px-4 py-2">LINK</Link>
+      <Route path="/test" component={Test} />
+    </Router>
   )
+}
+
+function Test() {
+  return <div>
+    <h1 className="text-3xl font-bold underline mb-6">
+      TestPage
+    </h1>
+    <Link href="/" className="bg-green-600 hover:bg-green-500 text-white rounded px-4 py-2">Return</Link>
+  </div>
 }
 
 export default App
